97aa6a6a91059bf10b2954e71377966d48588e95
Zamirbek <zamirbek.alimbekov3@gmail.com>
2022-10-12 12:45:44 +0600

    init

---
+++ js/index.js
@@ -0,0 +1,31 @@
+// Task1 from Samat
+/* вывести все value в консоль, 
+задача на 5-10мин, попалась Дену на собесе */
+const obj = {
+  value: 5,
+  next: {
+    value: 10,
+    next: {
+      value: 100,
+      next: {
+        value: 10000,
+        next: {
+          value: 20000,
+          next: {
+            value: 50000,
+            next: {
+              value: 10000000,
+              next: {
+                value: 20000000,
+                next: {
+                  value: 5,
+                  next: null,
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+};
